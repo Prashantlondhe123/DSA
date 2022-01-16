96067ac319516cf7a4b46b9918b974264956f746
Prashant Rajendra Londhe <90589964+Prashantlondhe123@users.noreply.github.com>
2022-01-16 22:20:48 +0530

    Create Linked List.py

---
+++ Linked List.py
@@ -0,0 +1,9 @@
+Python Library for Linked List
+
+Linked list is a simple data structure in
+ programming, which obviously is used to
+ store data and retrieve it accordingly.
+ To make it easier to imagine, it is more like a dynamic array
+ in which data elements are linked via pointers (i.e. the present record points
+ to its next record and the next one points to the record that comes after it, this goes on until
+ the end of the structure) rather than being tightly packed.
